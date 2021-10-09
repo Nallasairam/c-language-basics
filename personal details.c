064e0f4add0d2054578ff447c5e03ecbdf611f01
Nallasairam <92150333+Nallasairam@users.noreply.github.com>
2021-10-09 14:04:47 +0530

    Add files via upload

---
+++ personal details.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	printf("NAME:NALLA SAIRAM\n");
+	printf("FATHERNAME:NALLA SATYAGOPAL\n");
+	printf("MOTHERNAME:NALLA VARALAKSHMI\n");
+	printf("BROTHERNAME:NALLA THILAK\n");
+	
+	}
+	
+
