66b5d0c97f4ca3e600adf67c72cfbd0bfb08c859
Nallasairam <92150333+Nallasairam@users.noreply.github.com>
2021-10-20 13:55:43 +0530

    Add files via upload

---
+++ even and uneven.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+	int i,n;
+	for(i=20;i<=50;i++)
+	{
+	if(i%2 == 0)
+	{
+	
+	printf("%d is even\n",i);
+	}
+		
+	else
+	    {
+	    	printf("%d is uneven\n",i);
+		}
+}
+}
+	
+
