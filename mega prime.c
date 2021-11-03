3c2fa871239148dee718c487343ba7fe1c155e51
Nallasairam <92150333+Nallasairam@users.noreply.github.com>
2021-11-03 14:42:34 +0530

    Add files via upload

---
+++ mega prime.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+void main()
+{
+	int n;
+	scanf("%d",&n);
+	if (prime(n))
+	{
+		printf("prime number\n");
+		if(megaprime(n)==1)
+			{
+				printf("mega prime");
+			}
+			else
+			{
+				printf("not mega prime");
+			}
+	
+	}
+	else
+	{
+		printf("not prime");
+	}
+}
+int prime(int n)
+{
+	int i;
+	for(i=2;i<n;i++)
+	{
+		if (n%i==0)
+		{
+			return 0;
+		}
+	}
+	return 1;
+}
+int megaprime(int n)
+{
+  while(n!=0)
+
+	  {
+	  
+	    if(prime(n%10)==0)
+      {
+      		return 0;
+	  }
+	  	n=n/10;
+	  }
+	  
+	return 1;
+}
